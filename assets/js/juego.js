53b9c48b2ff175728fd3764135a822d0c92fd64e
HaroldDP <151193737+HaroldDP@users.noreply.github.com>
2023-11-27 09:59:58 -0500

    the last updated COMPUTER'S TURN, btnOrden, btnstop, add createletter function and other things

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,27 +1,45 @@
-(() =>{
+const myModule = (() =>{
     'use strict'
-    
+
     // VARAIBLE INITIALIZATION
     let deck = [];
-    const types = ['C','D','H','S'];
-    const specials = ['A','K','Q','J']
-    let playerPoints = 0,
-        computerPoints = 0;
-    
+
+    const types = ['C','D','H','S'],
+          specials = ['A','K','Q','J'];
+
+  
+    let playersdots = [];
+
+
     //REFERENCES HTML
-    const btnOrder = document.querySelector('#btnOrder');
-    const PointsHtml = document.querySelectorAll('small');
-    const btnStop = document.querySelector('#btnStop');
-    const btnNew = document.querySelector('#btnNew');
-    
+    const btnOrder = document.querySelector('#btnOrder'),
+          btnStop = document.querySelector('#btnStop'),
+          btnNew = document.querySelector('#btnNew');
+
     // create a  new letter in Html
-    const playerLetters = document.querySelector('#player-letters');
-    const Computerletters = document.querySelector('#Computer-letters');
+    const divLettersPlayers = document.querySelectorAll('.divletters'),
+    PointsHtml = document.querySelectorAll('small');
     
+
+     // initializeGame  
+     const initializeGame = (numPlyers = 2)=>{
+        deck = DeckCreate();
+        playersdots = [];
+        for (let i = 0; i < numPlyers; i++) {
+            playersdots.push(0);
+        }
+        PointsHtml.forEach(elem => elem.innerText = 0);
+        divLettersPlayers.forEach(elem => elem.innerHTML = '');
     
-    
+
+        btnStop.disabled = false;
+        btnOrder.disabled = false;
+    }
+
+                                                                
     //  DECK CREATION
     const DeckCreate = () =>{
+        deck = [];
         for( let i = 2; i<= 10; i++){
             for( let type of types){
                 deck.push(i+type);
@@ -35,13 +53,10 @@
             }
            
         }
-    
-       
-        deck = _.shuffle(deck);
-        return deck
+        
+        return _.shuffle(deck);
     }
     
-    DeckCreate();
     
     //  TAKING A LETTER
     const pedirCarta = () =>{
@@ -49,82 +64,100 @@
             throw '  there is not letters  on the deck';
          }
     
-         const letter = deck.pop()
-          return letter
-    
+         return  deck.pop();
+
     }
     
-    //  COMPUTER'S TURN
+    
+    // LETTER VALUE
+   const valorCarta = (letter) =>{
+        const value = letter.substring(0, letter.length-1); // Esta separando  el numero de la letra
+        return (isNaN(value))?// si 
+               (value ==='A')? 11:10 //si
+               : value*1; // sino
+   
+   }
+
+// Turn or shift = First player and  the las one  would be the computer
+    // ARRAY PLAYERS
+    const collectPoint = (letter,turn) =>{
+    playersdots[turn] = playersdots[turn]+valorCarta(letter);
+    PointsHtml[turn].innerText = playersdots[turn];
+    return playersdots[turn];
+
+   }
+
+   //CREATELETTER
+   const letterCreate = (letter, turn)=>{
+
+    const  Newletter =  document.createElement('img'); 
+    Newletter.classList.add('letter');
+    Newletter.src = `assets/cartas/${letter}.png`;
+    divLettersPlayers[turn].append(Newletter);
+
+   }
+   
+   const determineWinner = ()=>  {
+    //Destruction
+    const [minimunPointPlayer, computerPoints] = playersdots;
+
+       setTimeout(() => {
+    
+           if (computerPoints === minimunPointPlayer){
+               alert("tied");
+           }else if (minimunPointPlayer > 21){
+               alert("Computer was the  winner")
+           } else if(computerPoints > 21){
+               alert('User was the winner')
+           }else{
+               alert('Computer was the  winner')
+           }
+           
+       }, 100);
+       
+   }
+
+
+   //  COMPUTER'S TURN
       const computerTurn = (minimunPointPlayer)=>{
+        let computerPoints = 0;
+
         do {
     
             const letter = pedirCarta();
-            computerPoints = computerPoints+valorCarta(letter);
-            PointsHtml[1].innerText = computerPoints;
-    
-            
-            const  Newletter =  document.createElement('img'); 
-            Newletter.classList.add('letter');
-            Newletter.src = `assets/cartas/${letter}.png`;
-            Computerletters.append(Newletter);
-    
-            if(minimunPointPlayer > 21){
-                break;
-            }
+            computerPoints = collectPoint(letter , playersdots.length-1);
+            letterCreate(letter,playersdots.length-1);
             
         } while ((computerPoints < minimunPointPlayer) && (minimunPointPlayer<=21));
         
         //  DELAY RESULT. IT ISN'T MULTITHREAD
-        setTimeout(() => {
-    
-            if (computerPoints === minimunPointPlayer){
-                alert("tied");
-            }else if (minimunPointPlayer > 21){
-                alert("Computer was the  winner")
-            } else if(computerPoints > 21){
-                alert('User was the winner')
-            }else{
-                alert('Computer was the  winner')
-            }
-            
-        }, 10);
-    
+        determineWinner();
     
       }
     
     
-     // LETTER VALUE
-    const valorCarta = (letter) =>{
-         const value = letter.substring(0, letter.length-1); // Esta separando  el numero de la letra
-         return (isNaN(value))?// si 
-                (value ==='A')? 11:10 //si
-                : value*1; // sino
-    
-    }
     
     
     //EVENTOS
     btnOrder.addEventListener('click', ()=>{
         
             const letter = pedirCarta();
-            playerPoints = playerPoints+valorCarta(letter);
-            PointsHtml[0].innerText = playerPoints;
-    
+            const playersdots = collectPoint(letter,0);
+            letterCreate(letter,0);
+
             
-            const  Newletter =  document.createElement('img'); 
-            Newletter.classList.add('letter');
-            Newletter.src = `assets/cartas/${letter}.png`;
-            playerLetters.append(Newletter);
-    
-            if (playerPoints>21){
-                console.warn(' i am sory ,  You Lose');
+            if (playersdots>21){
+                console.warn(' i am sorry ,  You Lose');
                 btnOrder.disabled = true;
-                computerTurn(playerPoints);
-    
-            }else  if(playerPoints === 21){
-                console.warn(' 21  Great');
                 btnStop.disabled = true;
+                computerTurn(playersdots);
+    
+            }else  if(playersdots === 21){
+                console.warn(' 21  Great');
                 btnOrder.disabled = true;
+                btnStop.disabled = true;
+                computerTurn(playersdots);
+                
             }
     
     });
@@ -133,38 +166,27 @@
     
     // STOP BUTTON
     btnStop.addEventListener('click', () =>{
-        btnStop.disabled = true;
         btnOrder.disabled = true;
-        computerTurn(playerPoints);
+        btnStop.disabled = true;
+        
+        computerTurn(playersdots[0]);
+        
     });
     
     
     
     
     // CLEAR THE  GAME
-    btnNew.addEventListener('click', ()=>{
-    
-        console.clear();
-        deck = [];
-    
-        deck = DeckCreate();
-        PointsHtml[0].innerText = 0;
-        PointsHtml[1].innerText = 0;
-    
-        computerPoints = 0;    
-        playerPoints = 0,
+    // btnNew.addEventListener('click', ()=>{
+    //     initializeGame();
         
-        // reset img
-        playerLetters.innerHTML = '';
-        Computerletters.innerHTML = ''; 
-    
-        btnStop.disabled = false;
-        btnOrder.disabled = false;
-    });
+ 
+    // });
     
     
-    
-     
-    
+    return {
+       NewGame:  initializeGame
+    };
+
 })();
 
